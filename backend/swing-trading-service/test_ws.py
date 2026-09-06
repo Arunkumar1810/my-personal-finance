6c776a85d4b27596c562d84d36cc87f4febe6bd1
Arunkumar <arunkumar1.ak700@gamil.com>
2026-09-06 11:58:40 +0530

    refactor: remove temporary JSON data files and obsolete utility scripts

--- backend/swing-trading-service/test_ws.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import websockets
-
-async def test():
-    async with websockets.connect('ws://localhost:8000/ws/holdings') as ws:
-        msg = await ws.recv()
-        print(msg)
-
-asyncio.run(test())
